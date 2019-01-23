d306414f5cd14b7c1efb5bfe2095b813de89e450
Dmitrii Zakharov <dzakharov@splunk.com>
2019-01-22 19:55:41 -0800

    Bypassed the controller

--- client/src/components/Districts/districts.js
+++ client/src/components/Districts/districts.js
@@ -4,64 +4,89 @@ import "./districts.css"
 
 import React, { Component } from "react";
 import { Col, Row, Container } from "../Grid";
+import {List} from "../List";
+import Senator from "../Senator";
 import API from "../../utils/API";
+import axios from "axios";
 
 class Districts extends Component {
   state = {
     senators: []
   };
 
-  getSenators = () => {
-    API.getSenators()
-      .then(res =>
-        this.setState({
-          books: res.members
-        })
-      )
-      .catch(() =>
-        this.setState({
-          books: [],
-          message: "No Senators Found, Try a Different Query"
-        })
-      );
-  };
+  // getSenators = () => {
+  //   API.getSenators()
+  //     .then(res => {
+  //       console.log("RES:");
+  //       console.log(res);
+  //       this.setState({
+  //         senators: res
+  //       })}
+  //     )
+  //     .catch(() =>
+  //       this.setState({
+  //         senators: [],
+  //         message: "No Senators Found, Try a Different Query"
+  //       })
+  //     );
+  // };
+
+  // componentDidMount() {
+  //   this.getSenators();
+  // }
+
+  componentDidMount() {
+    axios({
+      method: 'get',
+      baseURL: 'https://api.propublica.org/congress/v1/116/senate/members.json',
+      headers: {'X-API-Key': 'kbvtlqxgtqEP4TbguBcbVICEbNTmsBy8f9r4owm6'}
+    }).then(res => {
+      // do something with data
+      console.log('api response in componentDidMount: ', res.data)
+      this.setState({
+        senators: res.data.results
+      })
+    })    
+    .catch(err => console.log(err));
+    }
+  
 
   render() {
     console.log("senators:");
     console.log(this.state.senators);
     return (
       <Container>
-        {/* <Row>
-            <Card title="Results">
+        <Row>
+            {/* </Row><Card title="Results"> */}
               {this.state.senators.length ? (
                 <List>
                   {this.state.senators.map(senators => (
                     <Senator
-                      key={book.id}
-                      title={book.volumeInfo.title}
-                      subtitle={book.volumeInfo.subtitle}
-                      link={book.volumeInfo.infoLink}
-                      authors={book.volumeInfo.authors.join(", ")}
-                      description={book.volumeInfo.description}
-                      image={book.volumeInfo.imageLinks.thumbnail}
-                      Button={() => (
-                        <button
-                          onClick={() => this.handleBookSave(book.id)}
-                          className="btn btn-primary ml-2"
-                        >
-                          Save
-                        </button>
-                      )}
+                      // key={book.id}
+                      // title={book.volumeInfo.title}
+                      // subtitle={book.volumeInfo.subtitle}
+                      // link={book.volumeInfo.infoLink}
+                      // authors={book.volumeInfo.authors.join(", ")}
+                      // description={book.volumeInfo.description}
+                      // image={book.volumeInfo.imageLinks.thumbnail}
+                      // Button={() => (
+                      //   <button
+                      //     onClick={() => this.handleBookSave(book.id)}
+                      //     className="btn btn-primary ml-2"
+                      //   >
+                      //     Save
+                      //   </button>
+                      // )}
                     />
                   ))}
                 </List>
               ) : (
                 <h2 className="text-center">{this.state.message}</h2>
               )}
-            </Card>
-          </Col>
+            {/* </Row></Card> */}
+          {/* </Col> */}
         </Row>
-        <Footer /> */}
+        {/* <Footer /> */}
       </Container>
     );
   }
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,7 +2,10 @@ import axios from "axios";
 
 export default {
   // Gets books from the Google API
-  getSenators: function(q) {
-    return axios.get("/api/propublica/senators");
+  getSenators: function() {
+    var data = axios.get("/api/propublica/senators");
+    console.log("DATA: ");
+    console.log(data);
+    return data;
   },
 };
