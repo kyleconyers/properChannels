bba6762443bc5358fb2a828d1a9bc6c82ea0c17c
kyleconyers <conyersky@gmail.com>
2019-01-26 13:59:18 -0800

    body format

--- db/models/messages.js
+++ db/models/messages.js
@@ -4,15 +4,6 @@
 const mongoose = require('mongoose');
 
 const messageSchema = mongoose.Schema({
-  forum_id: mongoose.Schema.ObjectId,
-  forum: { type: mongoose.Schema.ObjectId, ref: 'forum' },
-  user_id: mongoose.Schema.ObjectId,
-  user: { type: mongoose.Schema.ObjectId, ref: 'user' },
-  date: Date,
-  title: String,
-  content: Object,
-  favorites: Array,
-  tags: Array,
-});
+ 
 
 module.exports = mongoose.model('message', messageSchema);
