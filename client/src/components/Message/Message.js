54fb00e14243c74ec840323d16acc85c382ef135
kyleconyers <conyersky@gmail.com>
2019-01-25 17:25:45 -0800

    messageboard additions

--- client/src/components/Message/Message.js
+++ client/src/components/Message/Message.js
@@ -4,10 +4,13 @@ import "./Message.css";
 
 const Message = props => (
     <div>
-      <h3>{props.title}</h3>
-      <h3>{props.name}</h3> 
-      <h4>{props.category}</h4> 
-      <h4>{props.message}</h4> 
+
+       
+      {/* <h3>{props.district}</h3> */}
+      <h3>{props.title}</h3> 
+      <h4>{props.author}</h4> 
+      <h4>{props.body}</h4>
+      <h4>{props.category}</h4>  
     </div>
 )
 //(header) district ID/name
