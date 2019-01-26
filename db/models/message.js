be5fb94f0500488254c9a9986331bce654c91981
M. Pennella <michael.pennella@gmail.com>
2019-01-26 14:21:02 -0800

    Altered Message model

--- db/models/message.js
+++ db/models/message.js
@@ -1,18 +1,11 @@
-/**
- * message model
- */
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-const messageSchema = mongoose.Schema({
+const messageSchema = new Schema({
   forum_id: mongoose.Schema.ObjectId,
-  forum: { type: mongoose.Schema.ObjectId, ref: 'forum' },
   user_id: mongoose.Schema.ObjectId,
-  user: { type: mongoose.Schema.ObjectId, ref: 'user' },
   date: Date,
-  title: String,
-  content: Object,
-  favorites: Array,
-  tags: Array,
+  content: String
 });
 
 module.exports = mongoose.model('message', messageSchema);
