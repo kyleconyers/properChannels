4f6bc32dc33340b5f3ef6a38f555574050dceac2
kyleconyers <conyersky@gmail.com>
2019-01-31 17:02:14 -0800

    message board stuff

--- client/src/App.js
+++ client/src/App.js
@@ -262,7 +262,7 @@ class App extends Component {
 						</p>
 						</CenterLinks>
 
-						<CenterBody className="centerBody">
+						<CenterBody user= {this.state.user} className="centerBody">
 
 							<Route
 								exact
--- client/src/components/Board/Board.js
+++ client/src/components/Board/Board.js
@@ -5,6 +5,8 @@ import API from "../../utils/API";
 import { Col, Row, Container } from "../Grid";
 import { List } from "../List";
 import Message from "../Message";
+import Form from "../Form";
+import Card from "../Card";
 
 //props.messages
 // const fakeMessages = [
@@ -24,7 +26,9 @@ import Message from "../Message";
 //     }
 // ];
 
-
+// Array.filter( function( item ) {
+//   return item.uSState === 'WA'
+// }
 {/* <h3>{props.title}</h3>
       <h3>{props.name}</h3> 
       <h4>{props.category}</h4> 
@@ -34,12 +38,19 @@ import Message from "../Message";
      
     
     state = {
-        messages: []
+        messages: [],
+        currentUsState: ""
       };
+      //passvvvvv form_id or whatever message board we are on 
     componentDidMount() {
         this.getSavedMessage();
+        API.getSavedForum()
+        // .then(data => data.json())
+        .then(data => console.log(data.data));
+
       }
-    
+    //load data.data toooooo load this into state
+    //store array of possibble US states into STATEdom
     getSavedMessage = () => {
         API.getSavedMessage()
           .then(res =>{
@@ -50,24 +61,69 @@ import Message from "../Message";
           )
           .catch(err => console.log(err));
       };
+
+     //This Green Part needs to be turned into message section
+     ////////////////////////////////
+      handleInputChange = event => {
+        const { name, value } = event.target;
+        this.setState({
+          [name]: value
+        });
+        // console.log("inside change")
+      };
+   
+    handleMessageSave = id => {
+        // const message = this.state.message.find(message => message.id === id);
+      
+        console.log(this.props)
+        API.saveMessage({
+          //this.state.currentusstate
+          forum_id: "5c50c8d52bc45519e9f1b9b9",
+          user_id: this.props.user._id,
+          content: this.state.q,
+          date: new Date()
+        }).then(() => this.getSavedMessage());
+      };
+      // resUser._id
+     
+      handleFormSubmit = event => {
+        event.preventDefault();
+        this.handleMessageSave();
+        // this.db.insert();
+        console.log("inside sumbit")
+      };
+    
+      
+
+      /*
+      */
+    
       render() {
         return (
           <Container>
             
             <Row>
               <Col size="md-12">
+              <Card title="Post Content" icon="far fa-book">
+                  <Form
+                     handleInputChange={this.handleInputChange}
+                     handleFormSubmit={this.handleFormSubmit}
+                     q={this.state.q}
+                    />
+              </Card>  
 
+                  <MessageList 
+                    messages = {this.state.messages}
+                  />
 
-              <MessageList 
-                messages = {this.state.messages}
-              />
+              </Col>
+             </Row>
 
+            {/* <Row>
+              <Message message={this.state.messages[0]}>
               
-              
-              {/* <Message message={this.state.messages[0]}>
-                
-              </Message> */}
-                {/* <MessageList title="Results">
+              </Message> 
+                 <MessageList title="Results">
                   {this.state.message ? (
                     <List>
                       {this.state.message.map(message => (
@@ -86,9 +142,9 @@ import Message from "../Message";
                   ) : (
                     <h2 className="text-center">{this.state.message}</h2>
                   )}
-                </MessageList> */}
-              </Col>
-            </Row>
+                </MessageList> 
+               
+            </Row> */}
             
           </Container>
         );
